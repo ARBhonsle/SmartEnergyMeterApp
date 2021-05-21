db703ae82d056c2ecf9cf71e45465c3b5de560b1
Aditi Bhonsle <aditirbhonsle2824@gmail.com>
2021-05-22 04:18:48 +0530

    Graph Update

--- app/src/main/java/com/example/smartenergymeterapp/ViewGraphActivity.java
+++ app/src/main/java/com/example/smartenergymeterapp/ViewGraphActivity.java
@@ -1,5 +1,6 @@
 package com.example.smartenergymeterapp;
 
+/*
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,6 +19,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import com.example.smartenergymeterapp.BluetoothMainActivity.Parameters;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class ViewGraphActivity extends AppCompatActivity {
@@ -76,7 +78,6 @@ public class ViewGraphActivity extends AppCompatActivity {
 
     }
 
-    /*
     @Override
     protected void onResume() {
         super.onResume ( );
@@ -98,5 +99,84 @@ public class ViewGraphActivity extends AppCompatActivity {
             }
         } ).start ();
     }
-    */
+
+}
+
+package com.javasampleapproach.firebaserealtimedb;
+*/
+
+import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
+
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
+        import com.google.firebase.database.*;
+        import com.jjoe64.graphview.GraphView;
+        import com.jjoe64.graphview.Viewport;
+        import com.jjoe64.graphview.series.DataPoint;
+        import com.jjoe64.graphview.series.LineGraphSeries;
+        import com.example.smartenergymeterapp.BluetoothMainActivity.Parameters;
+
+        import java.util.ArrayList;
+        import java.util.Objects;
+
+public class ViewGraphActivity extends AppCompatActivity {
+    private static final String TAG = "ReadAndWriteSnippets";
+    //private ArrayList<Metrics> metrics;
+    private LineGraphSeries<DataPoint> voltageSeries;
+    private LineGraphSeries<DataPoint> currentSeries;
+    final Context mcontext = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_graph);
+
+        GraphView graph = findViewById ( R.id.view_graph );
+        voltageSeries = new LineGraphSeries<DataPoint>();
+        currentSeries = new LineGraphSeries<DataPoint>();
+        graph.addSeries (voltageSeries);
+        graph.addSeries (currentSeries);
+        graph.onDataChanged ( false,false );
+        Viewport viewport = graph.getViewport ();
+        viewport.setYAxisBoundsManual ( false );
+        viewport.setXAxisBoundsManual(false );
+        viewport.setScrollable ( false );
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance ( ).getReference ().child("parameters").child("metrics");
+
+        Button home = findViewById ( R.id.home );
+        home.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Move to adapter list
+                Intent intent = new Intent( ViewGraphActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    //Metrics metrics = ds.getValue(Metrics.class);
+                    //assert metrics != null;
+                    voltageSeries.appendData(new DataPoint( Double.parseDouble(ds.child("unixTime").getValue(String.class))
+                            ,Double.parseDouble(Objects.requireNonNull(ds.child("voltage").getValue(String.class)))),false,50);
+                    currentSeries.appendData(new DataPoint( Double.parseDouble(ds.child("unixTime").getValue(String.class))
+                            ,Double.parseDouble(Objects.requireNonNull(ds.child("current").getValue(String.class)))),false,50);
+                    Log.i("Graph",ds.child("current").getValue(String.class));
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+    }
 }
